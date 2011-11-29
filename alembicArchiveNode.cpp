cb8af0fc0c618837dcd61ebaf6f90129450f77bd
Ashley Retallack <ashley-r@blue-bolt.com>
2011-11-29 12:28:09 +0000

    add copywrite notice to BlueBolt source

--- alembicArchiveNode.cpp
+++ alembicArchiveNode.cpp
@@ -1,13 +1,43 @@
-//
-// Copyright (C) 2011 BlueBolt Ltd.
-// 
-// File: alembicArchiveNode.h
-//
-// Dependency Graph Node: alembicArchiveNode
-//
-// Author: Ashley Retallack
-//
+/**
+(c)2011 Bluebolt Ltd.  All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+* Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above
+copyright notice, this list of conditions and the following disclaimer
+in the documentation and/or other materials provided with the
+distribution.
+* Neither the name of BlueBolt nor the names of
+its contributors may be used to endorse or promote products derived
+from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+Author:Ashley Retallack - ashley-r@blue-bolt.com
+
+File:alembicArchiveNode.cpp
+
+Dependency Graph Node: alembicArchiveNode
+
+Based upon animaAlembicHolder by Olli Rajala @ Anima
+
+**/
+
 // TODO: Add viewport 2.0 compatibility
+// FIXME: Many dense archive nodes in scene crash maya when activating draw
 
 #include <maya/MCommandResult.h>
 #include <math.h>
@@ -31,7 +61,7 @@
 #include "boost/foreach.hpp"
 
 // The id is a 32bit value used to identify this type of node in the binary file format.
-MTypeId     alembicArchiveNode::id( 0x00114955 ); // something like 0x0018c04e - check /anima/maya/src/animaNodeIds.txt
+MTypeId     alembicArchiveNode::id( 0x00114955 );
 
 MObject     alembicArchiveNode::aAbcFile;
 MObject     alembicArchiveNode::aObjectPath;
@@ -77,7 +107,6 @@ void updateAbc(const void* data)
     alembicArchiveNode::alembicArchiveNode *node = (alembicArchiveNode::alembicArchiveNode *)data;
 
     MFnDagNode fn( node->thisMObject() );
-   //     std::cout << "plugDirtied: " << fn.name() << " " << plug.name() << " = " << plug.asString() << std::endl;
     MString file;
     MPlug fplug  = fn.findPlug( alembicArchiveNode::aAbcFile );
     fplug.getValue(file);
@@ -91,7 +120,6 @@ void updateAbc(const void* data)
 
     MString mkey = file+"/"+objectPath;
     std::string key = mkey.asChar();//node->getSceneKey();
-//        std::string key = node->getSceneKey();
 
     if (node->m_currscenekey != key) {
         alembicArchiveNode::abcSceneManager.removeScene(node->m_currscenekey);
@@ -106,10 +134,8 @@ void updateAbc(const void* data)
 void abcDirtiedCallback( MObject & nodeMO , MPlug & plug, void* data)
 {
     alembicArchiveNode::alembicArchiveNode *node = (alembicArchiveNode::alembicArchiveNode *)data;
-//    MFnDagNode fn( nodeMO );
     if (plug.attribute()==alembicArchiveNode::aAbcFile || plug.attribute()==alembicArchiveNode::aObjectPath) {
         MFnDagNode fn( node->thisMObject() );
-//        std::cout << "plugDirtied: " << fn.name() << " " << plug.name() << " = " << plug.asString() << std::endl;
         node->m_abcdirty = true;
     }
 }
